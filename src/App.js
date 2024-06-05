3cfe8970ff3aa4f624e7572539632aea62ca2552
Shariq Ansari <ansari.shariq.badshah@gmail.com>
2024-06-06 00:30:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 
-function App() {
+const App = () => {
+  const gsapref = useRef();
+
+  useEffect(() => {
+    gsap.to(gsapref.current, {
+      opacity: 1,
+      delay: 0.7,
+      rotate: 360,
+      x: 100,
+      duration: 2 // Duration of the rotation animation (in seconds)
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <div style={{marginTop:50}}>
+     <img ref={gsapref}
+        style={{ opacity: 0, height: 400 }}
+        src="https://i.pinimg.com/originals/f4/8f/ea/f48fea746a31d600627605dcfe902b58.png"
+        alt="wheel" />
+     </div>
     </div>
   );
 }
